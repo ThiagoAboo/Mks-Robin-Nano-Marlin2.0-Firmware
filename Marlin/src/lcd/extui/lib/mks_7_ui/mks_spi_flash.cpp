6936f31a95230e58a482aa621c3c1c42bfb7e58a
ThiagoAboo <74603323+ThiagoAboo@users.noreply.github.com>
2021-03-29 00:13:34 -0300

    Create mks_spi_flash.cpp
    criacao do mks ui com o lvgl7

---
+++ Marlin/src/lcd/extui/lib/mks_7_ui/mks_spi_flash.cpp
@@ -0,0 +1,76 @@
+/**
+ * Marlin 3D Printer Firmware
+ * Copyright (c) 2020 MarlinFirmware [https://github.com/MarlinFirmware/Marlin]
+ *
+ * Based on Sprinter and grbl.
+ * Copyright (c) 2011 Camiel Gubbels / Erik van der Zalm
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ */
+#include "../../../../inc/MarlinConfigPre.h"
+
+#if HAS_TFT_LVGL_7_UI
+
+#include "mks_main_ui.h"
+
+#include "../../../../MarlinCore.h" // for marlin_state
+#include "../../../../libs/W25Qxx.h"
+
+MKS_CFG_ITMES mks_CfgItems;
+
+void mks_load_spi_flash() {
+    
+    mks_CfgItems.theme_dark         = 0x02;
+    mks_CfgItems.wifi_mode_sel      = MKS_STA_MODEL;
+    mks_CfgItems.wifi_type          = MKS_ESP_WIFI;
+    mks_CfgItems.encoder_enable     = false;
+
+    #if HAS_ROTARY_ENCODER
+        mks_CfgItems.encoder_enable = true;
+    #endif
+
+    #if LV_USE_THEME_MATERIAL
+        mks_CfgItems.theme_dark     = LV_THEME_MATERIAL_FLAG_LIGHT;
+    #endif
+    
+    W25QXX.SPI_FLASH_BufferRead((uint8_t *)&mks_CfgItems.spi_flash_flag, MKS_VAR_INF_ADDR, sizeof(mks_CfgItems.spi_flash_flag));
+    if (mks_CfgItems.spi_flash_flag == MKS_FLASH_INF_VALID_FLAG) {
+        W25QXX.SPI_FLASH_BufferRead((uint8_t *)&mks_CfgItems, MKS_VAR_INF_ADDR, sizeof(mks_CfgItems));
+    }
+    else {
+        mks_CfgItems.spi_flash_flag = MKS_FLASH_INF_VALID_FLAG;
+        W25QXX.SPI_FLASH_SectorErase(MKS_VAR_INF_ADDR);
+        W25QXX.SPI_FLASH_BufferWrite((uint8_t *)&mks_CfgItems, MKS_VAR_INF_ADDR, sizeof(mks_CfgItems));
+        //init gcode command
+        W25QXX.SPI_FLASH_BufferWrite((uint8_t *)&mks_custom_gcode_command[0], MKS_AUTO_LEVELING_COMMAND_ADDR, 100);
+        W25QXX.SPI_FLASH_BufferWrite((uint8_t *)&mks_custom_gcode_command[1], MKS_OTHERS_COMMAND_ADDR_1, 100);
+        W25QXX.SPI_FLASH_BufferWrite((uint8_t *)&mks_custom_gcode_command[2], MKS_OTHERS_COMMAND_ADDR_2, 100);
+        W25QXX.SPI_FLASH_BufferWrite((uint8_t *)&mks_custom_gcode_command[3], MKS_OTHERS_COMMAND_ADDR_3, 100);
+        W25QXX.SPI_FLASH_BufferWrite((uint8_t *)&mks_custom_gcode_command[4], MKS_OTHERS_COMMAND_ADDR_4, 100);
+    }
+}
+
+void mks_update_spi_flash() {
+  uint8_t command_buf[512];
+
+  W25QXX.init(SPI_QUARTER_SPEED);
+  //read back the gcode command befor erase spi flash
+  W25QXX.SPI_FLASH_BufferRead((uint8_t *)&command_buf, MKS_GCODE_COMMAND_ADDR, sizeof(command_buf));
+  W25QXX.SPI_FLASH_SectorErase(MKS_VAR_INF_ADDR);
+  W25QXX.SPI_FLASH_BufferWrite((uint8_t *)&mks_CfgItems, MKS_VAR_INF_ADDR, sizeof(mks_CfgItems));
+  W25QXX.SPI_FLASH_BufferWrite((uint8_t *)&command_buf, MKS_GCODE_COMMAND_ADDR, sizeof(command_buf));
+}
+
+#endif
